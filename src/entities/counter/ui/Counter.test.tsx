b8f2c49b2fdc867c28161e1397c2ab4998270052
Artem <kadet_2003@list.ru>
2026-04-02 23:19:24 +0700

    create counter and aded unit-test

---
+++ src/entities/counter/ui/Counter.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Counter } from './Counter';
+
+describe('Counter component', () => {
+  test('отображает начальное значение 0', () => {
+    render(<Counter />);
+    const valueElement = screen.getByTestId('counter-value');
+    expect(valueElement).toHaveTextContent('0');
+  });
+
+  test('увеличивает значение при клике на Increment', () => {
+    render(<Counter />);
+    const incButton = screen.getByText('Increment');
+    fireEvent.click(incButton);
+    const valueElement = screen.getByTestId('counter-value');
+    expect(valueElement).toHaveTextContent('1');
+  });
+
+  test('уменьшает значение при клике на Decrement', () => {
+    render(<Counter />);
+    const decButton = screen.getByText('Decrement');
+    fireEvent.click(decButton);
+    const valueElement = screen.getByTestId('counter-value');
+    expect(valueElement).toHaveTextContent('-1');
+  });
+});
---
+++ src/entities/counter/ui/Counter.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 data-testid="counter-value">{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+};
