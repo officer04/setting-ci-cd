3a860bc08c98ebdd2fc146ee8fa223220d98863d
Artem <kadet_2003@list.ru>
2026-04-03 20:20:35 +0700

    refactoring workflows

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "setting-ci-cd",
   plugins: [react()],
   test: {
     globals: true,          
